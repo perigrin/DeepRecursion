56ce74bceab02c05f8e828e4c94a0b5922a08c32
Chris Prather <chris@prather.org>
2011-11-23 15:05:07 -0500

    simplify things if we can depend on the better-kioku-default branch of magpie being there

--- lib/DeepRecursion/Resource/User.pm
+++ lib/DeepRecursion/Resource/User.pm
@@ -4,12 +4,5 @@ extends "Magpie::Resource::Kioku";
 
 has '+wrapper_class' => ( default => 'DeepRecursion::Model::User', );
 
-sub _build_data_source {
-    my $self = shift;
-    my $k = $self->resolve_asset( service => 'kioku_dir' );
-    $self->_kioku_scope( $k->new_scope );
-    return $k;
-}
-
 1;
 __END__
