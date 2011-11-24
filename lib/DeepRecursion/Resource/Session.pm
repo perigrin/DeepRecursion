dc50aee2eacbc0a0b8bf0bd5ef588e5ffdb10d5c
Chris Prather <chris@prather.org>
2011-11-23 19:32:25 -0500

    add polish to the session handling and tests

--- lib/DeepRecursion/Resource/Session.pm
+++ lib/DeepRecursion/Resource/Session.pm
@@ -1,6 +1,6 @@
 package DeepRecursion::Resource::Session;
 use Moose;
-extends "Magpie::Resource::Kioku";
+extends qw(Magpie::Resource);
 
 use Magpie::Constants;
 use Try::Tiny;
@@ -8,19 +8,68 @@ use Plack::Session;
 use Data::Dumper::Concise;
 use Digest::SHA;
 
+has data_source => (
+    is      => 'ro',
+    isa     => 'KiokuDB',
+    lazy    => 1,
+    builder => '_build_data_source',
+    handles => { lookup_user => 'lookup', }
+);
+
+has _kioku_scope => (
+    is  => 'rw',
+    isa => 'KiokuDB::LiveObjects::Scope',
+);
+
+sub _build_data_source {
+    my $self = shift;
+    my $k    = try {
+        $self->resolve_asset( service => 'kioku_dir' );
+    }
+    catch {
+        my $error = "Could not connect to Kioku data source: $_\n";
+        warn $error;
+        $self->set_error( { status_code => 500, reason => $error } );
+    };
+
+    return undef if $self->has_error;
+    $self->_kioku_scope( $k->new_scope );
+    return $k;
+}
+
+has session => (
+    isa     => 'Plack::Session',
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_sessions'
+);
+
+sub _build_sessions { Plack::Session->new( shift->request->env ) }
+
 sub GET {
     my $self = shift;
 
     if ( my $action = $self->request->param('logout') ) {
         return $self->DELETE;
     }
-
+    if ( $self->request->path_info =~ qr|/session| ) {
+        my $session = $self->session;
+        my $session_id = ( split '/', $self->request->path_info )[-1];
+        if ( $session->id eq $session_id ) {
+            $self->response->redirect('/');
+            return DONE;
+        }
+        else {
+            $self->set_error( {status_code => 404, reason => "Couldn't find session $session_id"});
+            return OK;
+        }
+    }
     return OK;
 }
 
 sub DELETE {
     my $self    = shift;
-    my $session = Plack::Session->new( $self->request->env );
+    my $session = $self->session;
     $session->expire;
     $self->response->redirect('/login');
     return DONE;
@@ -31,14 +80,12 @@ sub POST {
     my $ctxt = shift;
     my $req  = $self->request;
 
-    my $session  = Plack::Session->new( $req->env );
+    my $session  = $self->session;
     my $username = $req->param('username');
     my $password = $req->param('password');
 
-    my $user = undef;
-
-    try {
-        $user = $self->data_source->lookup("user:${username}");
+    my $user = try {
+        $self->lookup_user("user:${username}");
     }
     catch {
         my $error = "Could not GET data from Kioku data source: $_\n";
@@ -48,7 +95,7 @@ sub POST {
     return OK if $self->has_error;
 
     unless ($user) {
-        $self->set_error(404);
+        $self->set_error( {status_code => 404, reason => "Couldn't find user $username."});
         return OK;
     }
 
@@ -60,8 +107,9 @@ sub POST {
     $session->set( user => $user );
     $self->state('created');
     $self->response->status(303);
-    $self->response->header( 'Location' => '/' );
+    $self->response->header( 'Location' => '/session/' . $session->id );
     return DONE;
 }
 
 1;
+__END__
---
+++ t/003_session.t
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Plack::Test;
+use HTTP::Request::Common;
+
+use DeepRecursion;
+
+my $app = DeepRecursion->new(
+    kioku_user => '',
+    kioku_pass => '',
+    kioku_dsn  => 'dbi:SQLite:site.db',
+)->app;
+
+test_psgi $app => sub {
+    my $cb = shift;
+
+    # Check Login Form
+    my $res = $cb->( GET '/login', Accept => 'text/html' );
+    for ( $res->content ) {
+        like $_, qr|<h1><a href="/">DeepRecursion</a></h1>|,
+            '/login has a DeepRecursion header';
+        like $_,
+            qr|<div class="input"><label>Username</label><input type="text" name="username"></div>|,
+            '...and a field for username';
+        like $_,
+            qr|<div class="input"><label>Password</label><input type="password" name="password"></div>|,
+            '...and a field for password';
+        like $_, qr|<div class="input"><input type="submit" name="go"></div>|,
+            '...and a submit button';
+    }
+
+    # Create Session
+    $res = $cb->(
+        POST '/login',
+        [ username => 'perigrin', password => 'pippin' ]
+    );
+    is $res->code, '303', 'got the expected code (303)';
+    like $res->header('Location'), qr|/session/[\w]+|,
+        'response location looks correct';
+
+    # Get the Session Resource
+    my $location = $res->header('Location');
+    my $cookie   = $res->header('Set-Cookie');
+    $res = $cb->( GET $location, Cookie => $cookie );
+    is $res->code, 302, 'got the expected code (302)';
+    like $res->header('Location'), qr|/|, 'response location looks correct';
+
+    # Get the new location
+    $location = $res->header('Location');
+    $cookie   = $res->header('Set-Cookie');
+    $res = $cb->( GET $location, Cookie => $cookie, Accept => 'text/html' );
+    is $res->code, 200, 'got the expected code (200)';
+    for ( $res->content ) {
+        like $_, qr|<h1><a href="/">DeepRecursion</a></h1>|,
+            "$location has DeepRecursion header";
+        like $_, qr|\Q<li><a href="/login?logout=1">Logout</a></li>\E|,
+            '...and a logout link';
+        like $_, qr|<h2>Hey there perigrin!</h2>|, '...and the greeting';
+        like $_, qr|<a href="/new_question">ask a question</a>|,
+            '...and a link to ask a question';
+    }
+
+};
+
+done_testing;
